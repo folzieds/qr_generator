5b206f3df534c467fdd67ac14b41bb5b6d0c5976
folzieds <folzieds@gmail.com>
2021-06-03 14:27:12 +0100

    Added a request body to the controller

--- src/main/java/com/phos/qr/service/QRCodeGenerator.java
+++ src/main/java/com/phos/qr/service/QRCodeGenerator.java
@@ -1,5 +1,6 @@
 package com.phos.qr.service;
 
+import com.google.gson.Gson;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.google.zxing.client.j2se.MatrixToImageWriter;
@@ -10,8 +11,7 @@ import com.phos.qr.models.QRImageData;
 import org.springframework.stereotype.Service;
 
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
+
 
 @Service
 public class QRCodeGenerator {
@@ -19,12 +19,13 @@ public class QRCodeGenerator {
     public BufferedImage generate(QrRequestData requestData) throws WriterException {
         QRCodeWriter qrCodeWriter = new QRCodeWriter();
         QRImageData imageData = new QRImageData();
+        Gson gson = new Gson();
 
-        // convert to json and encrypt
+        // encrypt
         imageData.setName(requestData.getName());
         imageData.setAmount(requestData.getAmount());
 
-        BitMatrix bitMatrix = qrCodeWriter.encode("imageData", BarcodeFormat.QR_CODE,200,200);
+        BitMatrix bitMatrix = qrCodeWriter.encode(gson.toJson(imageData), BarcodeFormat.QR_CODE,200,200);
 
         return MatrixToImageWriter.toBufferedImage(bitMatrix);
     }
